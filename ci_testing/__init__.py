6aecaf5a99448bea2d44b78a5bb4c3354028b5fb
Daniel Smith <malorian@me.com>
2017-09-28 10:40:14 -0400

    Added first file to ci_testing

---
+++ ci_testing/__init__.py
@@ -0,0 +1,5 @@
+"""
+The is the base file for ci_testing
+"""
+
+from .add import *
---
+++ ci_testing/add.py
@@ -0,0 +1,22 @@
+"""
+This is the add file.
+"""
+
+def add(x, y):
+    """
+    This is a add function
+
+    Parameters
+    ----------
+    x : {int, float, arb}
+        The first parameter to add
+    y : {int, float, arb}
+        The second parameter to add
+
+    Return
+    ------
+    ret : {int, float, arb}
+        The results of adding x and y
+    
+    """
+    return x + y
