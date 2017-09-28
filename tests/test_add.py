92882aadde9e29ab88d773d9a07552e487ac2499
Daniel Smith <malorian@me.com>
2017-09-28 11:16:35 -0400

    YAPF testing folder

--- tests/test_add.py
+++ tests/test_add.py
@@ -5,11 +5,12 @@ Tests the ci_testing add functionality
 import ci_testing as ct
 import pytest
 
+_add_tests = [
+    (4, 3, 7),
+    (5.3, 6.7, 12),
+    (5.3, 6.7, 12 + 1.e-14),
+]
 
-_add_tests = [(4, 3, 7),
-              (5.3, 6.7, 12),
-              (5.3, 6.7, 12 + 1.e-14),
-             ]
 
 @pytest.mark.parametrize("x,y,result", _add_tests)
 def test_add(x, y, result):
