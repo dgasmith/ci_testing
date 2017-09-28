63d3b23d64d409b2299079d599c0b7e571d22044
Daniel Smith <malorian@me.com>
2017-09-28 10:56:26 -0400

    Adds unittesting and a setup.py

---
+++ tests/test_add.py
@@ -0,0 +1,16 @@
+"""
+Tests the ci_testing add functionality
+"""
+
+import ci_testing as ct
+import pytest
+
+
+_add_tests = [(4, 3, 7),
+              (5.3, 6.7, 12),
+              (5.3, 6.7, 12 + 1.e-14),
+             ]
+
+@pytest.mark.parametrize("x,y,result", _add_tests)
+def test_add(x, y, result):
+    assert result == pytest.approx(ct.add(x, y))
